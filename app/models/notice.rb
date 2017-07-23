054f0077cb2d6b0a74557608f108c4dfc1eab26c
Md. Ashikuzzaman <md.ashikuzzaman.1429@gmail.com>
2017-07-23 18:06:37 +0600

    trying another model named Notice specs for the models are written 33 examples, 0 failure

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,2 +1,6 @@
+# app/models/notice.rb
+
 class Notice < ApplicationRecord
+	# validations
+  validates_presence_of :notice_for, :details, :important_dates, :attachments_needed
 end
--- spec/models/notice_spec.rb
+++ spec/models/notice_spec.rb
@@ -1,5 +1,12 @@
+# spec/models/notice_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe Notice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  # ensure columns notice_for, details, important_dates and attachments_needed are present before saving
+  it { should validate_presence_of(:notice_for) }
+  it { should validate_presence_of(:details) }
+  it { should validate_presence_of(:important_dates) }
+  it { should validate_presence_of(:attachments_needed) }
 end
